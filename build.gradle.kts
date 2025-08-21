600e7255c0e255590f94b5dee3351fb039a69e24
thaomaitam <thaomtam@gmail.com>
2025-08-21 17:00:29 +0700

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,10 @@
-// build.gradle.kts (Root)
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+tasks.register("Delete", Delete::class) {
+    delete(rootProject.buildDir)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,53 +1,18 @@
-// settings.gradle.kts
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        
-        // DexKit repository
-        maven {
-            url = uri("https://maven.pkg.github.com/LuckyPray/DexKit")
-            credentials {
-                username = providers.gradleProperty("gpr.user").orNull 
-                    ?: System.getenv("GITHUB_ACTOR")
-                password = providers.gradleProperty("gpr.key").orNull 
-                    ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-        
-        // JitPack for GitHub dependencies
-        maven { url = uri("https://jitpack.io") }
-        
-        // Local Maven
-        mavenLocal()
-    }
-    
-    // Enable version catalogs
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
+        maven(url = "https://api.xposed.info/")
     }
 }
-
 rootProject.name = "Cpatcher"
 include(":app")
-
-// Gradle optimizations
-enableFeaturePreview("CONFIGURATION_CACHE")
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+include(":hidden-api")
