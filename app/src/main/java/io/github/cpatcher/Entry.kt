9ff2961b97b28e637645a6ba73c8fde639ac40db
thaomaitam <thaomtam@gmail.com>
2025-08-26 21:07:22 +0700

    Add files via upload

--- app/src/main/java/io/github/cpatcher/Entry.kt
+++ app/src/main/java/io/github/cpatcher/Entry.kt
@@ -7,7 +7,6 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage
 import io.github.cpatcher.bridge.LoadPackageParam
 import io.github.cpatcher.handlers.QslockHandler
 import io.github.cpatcher.handlers.TermuxHandler
-import io.github.cpatcher.handlers.ScreenshotEnablerHandler
 
 class Entry : IXposedHookLoadPackage, IXposedHookZygoteInit {
     companion object {
@@ -28,25 +27,6 @@ class Entry : IXposedHookLoadPackage, IXposedHookZygoteInit {
         logI("Cpatcher: ${lpparam.packageName} ${lpparam.processName}")
         val handler = when (lpparam.packageName) {
             "com.termux" -> TermuxHandler()
-            "android" -> {
-    when (lpparam.processName) {
-        "android" -> {
-            // system_server process - Deploy framework hooks
-            logI("Injecting into system_server (PID: ${android.os.Process.myPid()})")
-            ScreenshotEnablerHandler()
-        }
-        "com.android.systemui" -> {
-            // SystemUI process - Different hook strategy
-            logI("Detected SystemUI subprocess - skipping framework hooks")
-            return
-        }
-        else -> {
-            // Unknown android package subprocess
-            logW("Unknown android subprocess: ${lpparam.processName}")
-            return
-        }
-    }
-}
             "com.android.systemui" -> {
                 if (lpparam.processName == "com.android.systemui") QslockHandler
                 else return
